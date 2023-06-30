766931fbddb61aaee5bd3ad825fc0ce1d0f442c2
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-30 17:44:51 +0530

    added Styling

--- pages/index.js
+++ pages/index.js
@@ -1,128 +1,123 @@
-import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import axios from "axios";
+// import { useEffect, useRef, useState } from "react";
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+// import axios from "axios";
+
+import Completed from "@/Component/Completed";
 
 export default function Home() {
-  const [data, setData] = useState([]);
-  const todoRef = useRef();
-  const [editingItemId, setEditingItemId] = useState(null);
-  const router = useRouter();
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("/api/completedtask");
-      console.log(response.data);
-      setData(response.data);
-    } catch (error) {
-      console.log("Error:", error.message);
-    }
-  };
-  useEffect(() => {
-    
-    fetchData();
-  }, []);
-
-  const submitHandler = async (event) => {
-    event.preventDefault();
-    const inputData = {
-      todo: todoRef.current.value,
-      isCompleted: true,
-    };
-
-    try {
-      if (editingItemId) {
-        // Perform PUT request for editing an item
-        const response = await axios.put ("/api/completedtask", {
-          id: editingItemId,
-          data: inputData,
-        });
-        console.log("Item updated successfully:", response.data);
-        setEditingItemId(null); // Clear the editingItemId
-      } else {
-        // Perform POST request for adding a new item
-        const response = await axios.post("/api/completedtask", inputData);
-        console.log(response.data);
-      }
-
-      fetchData();
-      todoRef.current.value = ""; // Clear the input field
-    } catch (error) {
-      console.log("Error:", error.message);
-    }
-  };
-
-  const handleCheckboxChange = async(id) => {
-   
-    try {
-      const updatedData = data.map((item) => {
-        if (item._id === id) {
-          return {
-            ...item,
-            isCompleted: !item.isCompleted,
-          };
-        }
-        return item;
-      });
-      setData(updatedData);
-
-      const updatedItem = updatedData.find((item) => item._id === id);
-
-      const response = await axios.put("/api/completedtask", {
-        id: id,
-        data: updatedItem,
-      });
-
-      if (response.status === 200) {
-        console.log("Item updated successfully:", response.data);
-      } else {
-        console.log("Error updating item:", response.status);
-      }
-    } catch (error) {
-      console.log("Error:", error.message);
-    }
-  };
-
-  // const completedTasks = data.filter((item) => item.checked);
-
-  // const handleGoToCompletedTasks = () => {
-  //   router.push({
-  //     pathname: "/completedtask",
-  //     query: { data: JSON.stringify(data) },
-  //   });
+  // const [data, setData] = useState([]);
+  // const todoRef = useRef();
+  // const [editingItemId, setEditingItemId] = useState(null);
+  // const router = useRouter();
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get("/api/completedtask");
+  //     console.log(response.data);
+  //     setData(response.data);
+  //   } catch (error) {
+  //     console.log("Error:", error.message);
+  //   }
   // };
-  const fetchFunc = async () => {
-    const response = await axios.get("/api/completedtask");
-    console.log(response);
-  };
+  // useEffect(() => {
+    
+  //   fetchData();
+  // }, []);
 
-  const deleteHandler = async (id) => {
-    try {
-      const response = await axios.delete(`/api/completedtask?id=${id}`);
-      console.log(response.data);
-      if (response.status === 200) {
-        fetchData();
-      } else {
-        console.log("Error:", response.status);
-      }
-    } catch (error) {
-      console.log("Error:", error.message);
-    }
-  };
+  // const submitHandler = async (event) => {
+  //   event.preventDefault();
+  //   const inputData = {
+  //     todo: todoRef.current.value,
+  //     isCompleted: true,
+  //   };
 
-  const editbtnhandler = (id) => {
-    const selectedItem = data.find((item) => item._id === id);
-    if (selectedItem) {
-      todoRef.current.value = selectedItem.todo;
-      setEditingItemId(id);
-    }
-  };
+  //   try {
+  //     if (editingItemId) {
+  //       // Perform PUT request for editing an item
+  //       const response = await axios.put ("/api/completedtask", {
+  //         id: editingItemId,
+  //         data: inputData,
+  //       });
+  //       console.log("Item updated successfully:", response.data);
+  //       setEditingItemId(null); // Clear the editingItemId
+  //     } else {
+  //       // Perform POST request for adding a new item
+  //       const response = await axios.post("/api/completedtask", inputData);
+  //       console.log(response.data);
+  //     }
+
+  //     fetchData();
+  //     todoRef.current.value = ""; // Clear the input field
+  //   } catch (error) {
+  //     console.log("Error:", error.message);
+  //   }
+  // };
+
+  // const handleCheckboxChange = async(id) => {
+   
+  //   try {
+  //     const updatedData = data.map((item) => {
+  //       if (item._id === id) {
+  //         return {
+  //           ...item,
+  //           isCompleted: !item.isCompleted,
+  //         };
+  //       }
+  //       return item;
+  //     });
+  //     setData(updatedData);
+
+  //     const updatedItem = updatedData.find((item) => item._id === id);
+
+  //     const response = await axios.put("/api/completedtask", {
+  //       id: id,
+  //       data: updatedItem,
+  //     });
+
+  //     if (response.status === 200) {
+  //       console.log("Item updated successfully:", response.data);
+  //     } else {
+  //       console.log("Error updating item:", response.status);
+  //     }
+  //   } catch (error) {
+  //     console.log("Error:", error.message);
+  //   }
+  // };
+
+ 
+  // const fetchFunc = async () => {
+  //   const response = await axios.get("/api/completedtask");
+  //   console.log(response);
+  // };
+
+  // const deleteHandler = async (id) => {
+  //   try {
+  //     const response = await axios.delete(`/api/completedtask?id=${id}`);
+  //     console.log(response.data);
+  //     if (response.status === 200) {
+  //       fetchData();
+  //     } else {
+  //       console.log("Error:", response.status);
+  //     }
+  //   } catch (error) {
+  //     console.log("Error:", error.message);
+  //   }
+  // };
+
+  // const editbtnhandler = (id) => {
+  //   const selectedItem = data.find((item) => item._id === id);
+  //   if (selectedItem) {
+  //     todoRef.current.value = selectedItem.todo;
+  //     setEditingItemId(id);
+  //   }
+  // };
   
   
   
 
   return (
     <>
-      <h1>Todo List</h1>
+      {/* <h1>Todo List</h1>
       <div>
         <form onSubmit={submitHandler}>
           <label>Enter Todo </label>
@@ -148,7 +143,8 @@ export default function Home() {
         </ul>
       )}
 
-      <button onClick={fetchFunc}>See completed Task</button>
+      <button onClick={fetchFunc}>See completed Task</button> */}
+      <Completed/>
     </>
   );
 }
